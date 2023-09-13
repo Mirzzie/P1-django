84a8f57f84e3262c706f01585db20a37642d316c
Mirzzie <mirzad.ismail@proton.me>
2023-09-13 14:33:32 +0530

    rename login -> signin and dependent changes

--- p1/app/urls.py
+++ p1/app/urls.py
@@ -3,6 +3,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('signup/',views.signup, name='signup'),
-    path('login/',views.login, name='login'),
+    path('signin/',views.signin, name='signin'),
     path('cart/',views.cart, name='cart'),
 ]
