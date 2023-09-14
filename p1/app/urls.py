e93d0b9e0c7731b5dd37b2e582e23e9af9f65f55
Mirzzie <mirzad.ismail@proton.me>
2023-09-14 23:01:19 +0530

    initial mysql database integrations and changes

--- p1/app/urls.py
+++ p1/app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 from . import views
+
 urlpatterns = [
     path('', views.index, name='index'),
-    path('signup/',views.signup, name='signup'),
-    path('signin/',views.signin, name='signin'),
-    path('cart/',views.cart, name='cart'),
+    path('signup/', views.signup, name='signup'),
+    path('signin/', views.signin, name='signin'),
+    path('logout/', views.logout_user, name='logout'),
+    path('cart/', views.cart, name='cart'),
 ]
--- p1/app/views.py
+++ p1/app/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 
 # Create your views here.
@@ -6,22 +9,49 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'index.html')
 
+
 def signup(request):
     if request.method == 'POST':
-        Name = request.post['Name']
-        Username = request.post['username']
-        Email = request.post['Email']
-        Password = request.post['Password']
-        Confirm_password = request.post['cpassword']
+        name = request.POST['name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['cpassword']
+
+        if password != confirm_password:
+            return render(request, 'signup.html')
+        messages.error(request, "Password do not match !!")
+
+        user = User.objects.create_user(username, email, password)
+        user.save()
+
+        return redirect('signin')
     else:
         return render(request, 'signup.html')
 
+
 def signin(request):
-     if request.method == 'POST':
-        Username = request.post['username']
-        Password = request.post['Password']
-     else:
-      return render(request, 'signin.html')
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "signed in success!!")
+            return redirect('index')
+        else:
+            messages.error(request, "Password do not match !!")
+            return render(request, 'signin.html')
+    else:
+        return render(request, 'signin.html')
+
 
 def cart(request):
     return render(request, 'cart.html')
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "logged out!!")
+    return redirect('signin')
